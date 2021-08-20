18fc4588215a318fea4cc04b3ed22fbfb79e095d
ScuroGuardiano <scuroguardiano@gmail.com>
2021-08-20 23:05:27 +0000

    Added selection to table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, Output, ViewEncapsulation, EventEmitter } from '@angular/core';
-import { ColumnMode } from '@swimlane/ngx-datatable';
+import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
 import IColumns from './interfaces/columns';
 import ITableDeleteEvent from './interfaces/delete-event';
 import IRow from './interfaces/row';
@@ -14,15 +14,31 @@ export class TableComponent implements OnInit {
 
   constructor() { }
 
+  // #region Inputs
   @Input() rows: IRow[] = [];
   @Input() columns: IColumns = {};
   @Input() perPage: number = 10;
   @Input() deletable: boolean = false;
   @Input() editable: boolean = false;
+  @Input() selectable: boolean = false;
+  // #endregion
 
+  // #region Outputs
   @Output() delete = new EventEmitter<ITableDeleteEvent>();
+  // #endregion
+
+  // #region Two-way data
+  @Input() selected: IRow[] = [];
+  @Output() selectedChange = new EventEmitter<IRow[]>();
+
+  onSelect({ selected }: { selected: IRow[] }) {
+    this.selected = selected;
+    this.selectedChange.emit(this.selected);
+  }
+  // #endregion
 
   ColumnMode = ColumnMode;
+  SelectionType = SelectionType;
 
   ngOnInit(): void {
   }
